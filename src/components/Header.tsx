c81377c952f9f2a814b9d832c4390e1439fbeeb3
JJyen <gdb1632@naver.com>
2026-04-29 01:52:56 +0900

    style: Header.tsx 요소 비율 조정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <header
-      className="absolute top-0 left-0 w-full h-[253px] overflow-hidden bg-cover bg-[50%_50%]"
+      className="absolute top-0 left-0 w-full h-[230px] overflow-hidden bg-cover bg-[50%_50%]"
       style={{ backgroundImage: `url(${headerBg})` }}
     >
       <div
@@ -22,7 +22,7 @@ const Header = () => {
       />
       <div className="absolute top-0 left-0 w-full h-full">
         <div className="h-[64px] w-full flex justify-between items-center px-16">
-          <div className="font-bold text-[#f6f6f6] text-2xl">AI PROJECT</div>
+          <div className="font-bold text-[#f6f6f6] text-xl">AI PROJECT</div>
           <nav aria-label="주요 메뉴" className="flex justify-between w-[230px]">
             {menus.map((item) => (
               <button
@@ -42,7 +42,7 @@ const Header = () => {
           <form
             onSubmit={onSearch}
             role="search"
-            className="mt-8 h-12 w-[820px] flex items-center bg-[#ffffff26] rounded-[30px] overflow-hidden border border-solid border-[#cbcbcba1] backdrop-blur-[12.5px]"
+            className="mt-6 h-11 w-[800px] flex items-center bg-[#ffffff26] rounded-[30px] overflow-hidden border border-solid border-[#cbcbcba1] backdrop-blur-[12.5px]"
           >
             <input
               id={searchId}
