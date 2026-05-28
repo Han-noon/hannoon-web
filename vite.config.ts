f864f20823580a93875db8421707d22ef00eaa14
minji990 <rlaalswl6749@gamil.com>
2026-05-28 10:49:18 +0900

    feat: 타임라인 페이지 api 연결

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  optimizeDeps: {
-    include: [
-      'element-plus/es/components/row/style/index',
-      'element-plus/es/components/col/style/index',
-    ],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
