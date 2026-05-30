3e632f43bcb1e4efbfca92e0ee9019750d36527a
JJyen <gdb1632@naver.com>
2026-05-30 20:48:30 +0900

    feat: 이벤트 요약 페이지 누락 데이터 추가 바인딩

--- src/pages/event-summary/components/main/statistics/BiasInfo.tsx
+++ src/pages/event-summary/components/main/statistics/BiasInfo.tsx
@@ -1,6 +1,11 @@
+import useClickOutside from '@/hooks/useClickOutside';
 import type { EventSummary } from '@/types/eventSummary';
+import { useCallback, useRef, useState } from 'react';
 
 const BiasInfo = ({ event }: { event: EventSummary }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [bias, setBias] = useState<'left' | 'mid' | 'right' | null>(null);
+
   const total: number = event.left_count + event.mid_count + event.right_count;
 
   const left: number =
@@ -9,6 +14,30 @@ const BiasInfo = ({ event }: { event: EventSummary }) => {
     event.mid_count && total ? Math.ceil((event.mid_count / total) * 1000) / 10 : 0;
   const right: number =
     event.right_count && total ? Math.ceil((event.right_count / total) * 1000) / 10 : 0;
+
+  function handlePublishersModal(e: React.MouseEvent, newBias: 'left' | 'mid' | 'right') {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+
+    console.log(bias, newBias);
+
+    if (bias === newBias) {
+      setIsOpen(false);
+      setBias(null);
+      return;
+    }
+
+    setIsOpen(true);
+    setBias(newBias);
+  }
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+    setBias(null);
+  }, []);
+
+  const PublishersModalRef = useRef<HTMLDivElement | null>(null);
+  useClickOutside(PublishersModalRef, closeModal);
 
   return (
     <section className="w-full">
@@ -19,24 +48,55 @@ const BiasInfo = ({ event }: { event: EventSummary }) => {
           <p>중도</p>
           <p>보수</p>
         </div>
-        <div className="flex justify-between h-3 rounded-xl overflow-hidden mt-2 mb-6 bg-[#D9D9D9]">
-          <p style={{ width: `${left}%` }} className="bg-blue-600"></p>
-          <p style={{ width: `${mid}%` }} className="bg-purple-600"></p>
-          <p style={{ width: `${right}%` }} className="bg-red-600"></p>
-        </div>
-        <div className="text-xs flex justify-center gap-6 pb-6">
-          <p>
-            <span className="text-blue-600">●</span>
-            {left}%
-          </p>
-          <p>
-            <span className="text-purple-600">●</span>
-            {mid}%
-          </p>
-          <p>
-            <span className="text-red-600">●</span>
-            {right}%
-          </p>
+        <div ref={PublishersModalRef} className="relative">
+          <div className="flex justify-between h-3 rounded-xl overflow-hidden mt-2 mb-6 bg-[#D9D9D9] hover:cursor-pointer">
+            <p
+              style={{ width: `${left}%` }}
+              className="bg-blue-600 rounded-l-full box-border hover:border-2 border-blue-700"
+              onClick={(e) => handlePublishersModal(e, 'left')}
+            ></p>
+            <p
+              style={{ width: `${mid}%` }}
+              className={`bg-purple-600 box-border hover:border-2 border-purple-700 ${left === 0 ? 'rounded-l-full' : ''} ${right === 0 ? 'rounded-r-full' : ''}`}
+              onClick={(e) => handlePublishersModal(e, 'mid')}
+            ></p>
+            <p
+              style={{ width: `${right}%` }}
+              className="bg-red-600 rounded-r-full box-border hover:border-2 border-red-700"
+              onClick={(e) => handlePublishersModal(e, 'right')}
+            ></p>
+          </div>
+
+          {isOpen && (
+            <div
+              className={`box-border text-xs absolute top-5 border-2 bg-white w-full p-3 rounded-xl grid grid-cols-6 md:grid-cols-3 gap-y-5 z-50
+                  ${bias === 'right' ? 'border-red-600' : bias === 'mid' ? 'border-purple-600' : 'border-blue-600'}
+                `}
+            >
+              {bias &&
+                event.publishers[bias]?.map((publishers) => (
+                  <div key={publishers} className="flex flex-col items-center">
+                    <p className="bg-gray-400 w-9 h-9 rounded-full mb-1"></p>
+                    <p>{publishers}</p>
+                  </div>
+                ))}
+            </div>
+          )}
+
+          <div className="text-xs flex justify-center gap-6 pb-6">
+            <p>
+              <span className="text-blue-600">●</span>
+              {left}%
+            </p>
+            <p>
+              <span className="text-purple-600">●</span>
+              {mid}%
+            </p>
+            <p>
+              <span className="text-red-600">●</span>
+              {right}%
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/event-summary/components/main/statistics/TimelineSummaryInfo.tsx
+++ src/pages/event-summary/components/main/statistics/TimelineSummaryInfo.tsx
@@ -1,18 +1,24 @@
 import ShortcutButton from '@/components/ShortcutButton';
 import type { EventSummary } from '@/types/eventSummary';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const TimelineSummaryInfo = ({ event }: { event: EventSummary }) => {
   const textStyle = (title: string | null) => `
-  text-gray-600 truncate 
-  ${title === null ? '' : 'underline underline-offset-4 hover:text-black'}
-`;
+  text-gray-600 truncate hover:cursor-pointer
+  ${title === null ? 'hover:cursor-default' : 'underline underline-offset-4 hover:text-blue-500'}`;
+
+  const navigate = useNavigate();
+  function handleNavigate(eventId: number | null) {
+    if (!eventId) return;
+    console.log('id: ', eventId);
+    navigate(`/event-detail/${eventId}`);
+  }
 
   return (
     <section className="w-full">
       <p className="border-y border-gray47 py-3 text-center">타임라인</p>
       <div className="my-8 text-center">
-        <p className="mb-6">소속 토픽: 누락</p>
+        <p className="mb-6">소속 토픽: {event.topic_title}</p>
         <div className="md:min-w-[331px] flex h-36 text-sm relative px-2">
           <p className="absolute left-1/2 -translate-x-[55%] top-[13px] text-xs text-gray-400">●</p>
           <p className="absolute left-1/2 -translate-x-[55%] top-[63px] text-xs">●</p>
@@ -20,23 +26,26 @@ const TimelineSummaryInfo = ({ event }: { event: EventSummary }) => {
             ●
           </p>
           <div className="w-1/2 h-full border-r-2 border-gray-400 flex flex-col justify-between py-3 pr-4 text-right">
-            <Link to="#">
-              <p className={textStyle(event.prev_event)}>
-                {event.prev_event === null ? '정보없음' : event.prev_event}
-              </p>
-            </Link>
-            <Link to="#">
-              <p className={textStyle(event.next_event)}>
-                {event.next_event === null ? '정보없음' : event.next_event}
-              </p>
-            </Link>
+            <p
+              onClick={() => handleNavigate(event.prev_event_id)}
+              className={textStyle(event.prev_event_title)}
+            >
+              {event.prev_event_title === null ? '정보없음' : event.prev_event_title}
+            </p>
+            <p
+              onClick={() => handleNavigate(event.next_event_id)}
+              className={textStyle(event.next_event_title)}
+            >
+              {event.next_event_title === null ? '정보없음' : event.next_event_title}
+            </p>
           </div>
           <div className="w-1/2 h-full flex items-center pl-4">
-            <p className="truncate">{event.title}</p>
+            <p className="truncate">{event.event_title}</p>
           </div>
         </div>
-      </div>
-      <ShortcutButton path="/timeline" name="전체 타임라인 보기" />
+      </div>{' '}
+      {/*토픽 아이디로 바꿔야 함!*/}
+      <ShortcutButton path={`/timeline/${event.event_id}`} name="전체 타임라인 보기" />
     </section>
   );
 };
