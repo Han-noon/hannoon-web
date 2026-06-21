b6619f66c2a798db39525001c875d49f3a6ff85c
JJyen <gdb1632@naver.com>
2026-06-22 05:25:04 +0900

    style: 토픽 리스트, 타임라인 페이지 디자인 수정

--- src/pages/TimelinePage.tsx
+++ src/pages/TimelinePage.tsx
@@ -2,18 +2,68 @@ import { useState, useEffect, useRef } from 'react';
 import { getEventsByTopic } from '@/api/event/getEventsByTopic';
 import { getTopicById } from '@/api/topic/getTopicById';
 import type { TopicResponse } from '@/api/topic/getTopicById';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import type { EventResponse } from '@/types/timeline';
+import { useLocation } from 'react-router-dom';
+import useSession from '@/hooks/useSession';
+import { subscribeTopic } from '@/api/topic/subscribeTopic';
+import { unsubscribeTopic } from '@/api/topic/unsubscribeTopic';
+import SubscribeModal from '@/components/SubscribeModal';
+import NotificationPermissionModal from '@/components/NotificationPermissionModal';
 
 const TimelinePage = () => {
-  const [showToast, setShowToast] = useState(false);
+  //const [showToast, setShowToast] = useState(false);
   const [isAscending, setIsAscending] = useState(true);
   const [selectedId, setSelectedId] = useState<number | null>(null);
   const [eventsdata, setEventsdata] = useState<EventResponse | null>(null);
   const [topicData, setTopicData] = useState<TopicResponse | null>(null);
-  const articleRef = useRef<HTMLElement>(null);
 
   const { topic_id } = useParams();
+  // 임시: 토픽 조회 시 구독 정보 필요
+  const { state } = useLocation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showPermissionModal, setShowPermissionModal] = useState(false);
+  const [subscribe, setSubscribe] = useState(state.is_subscribed);
+
+  const { session } = useSession();
+  const navigate = useNavigate();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      if (confirm('로그인이 필요한 기능입니다. 로그인하시겠습니까?')) return navigate('/signin');
+      else return;
+    }
+
+    if (!subscribe) {
+      let permission = Notification.permission;
+
+      if (permission === 'default') {
+        permission = await Notification.requestPermission();
+      }
+
+      if (permission !== 'granted') {
+        setShowPermissionModal(true);
+        return;
+      }
+
+      const data = await subscribeTopic(Number(topic_id));
+
+      setSubscribe(data.is_subscribed);
+
+      if (data.is_subscribed) {
+        setIsModalOpen(true);
+      }
+    } else {
+      try {
+        await unsubscribeTopic(Number(topic_id));
+        setSubscribe(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const articleRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     const fetchTopic = async () => {
@@ -39,6 +89,7 @@ const TimelinePage = () => {
         });
 
         setEventsdata(data);
+        setSelectedId(data.events[0].id);
         console.log('결과:', data);
       } catch (error) {
         console.error('데이터 페칭 실패:', error);
@@ -48,10 +99,10 @@ const TimelinePage = () => {
     fetchEvents();
   }, [topic_id, isAscending]);
 
-  const handleAlertClick = () => {
-    setShowToast(true);
-    setTimeout(() => setShowToast(false), 3000);
-  };
+  // const handleAlertClick = () => {
+  //   setShowToast(true);
+  //   setTimeout(() => setShowToast(false), 3000);
+  // };
 
   const toggleSortOrder = () => {
     setIsAscending((prev) => !prev);
@@ -61,47 +112,49 @@ const TimelinePage = () => {
     eventsdata?.events.find((item) => item.id === selectedId) || eventsdata?.events[0];
 
   return (
-    <main className="w-full max-w-[1300px] mx-auto bg-white min-h-screen text-black pb-20 relative font-sans">
-      {showToast && (
+    <main className="w-full max-w-[1150px] mx-auto bg-white min-h-screen text-black pb-20 relative font-sans">
+      {/* {showToast && (
         <div className="text-center w-3/4 md:w-auto text-sm md:text-base px-6 py-3 fixed bottom-10 left-1/2 -translate-x-1/2 bg-[#474747] text-white rounded-full shadow-lg z-50 transition-opacity duration-300">
           실시간 알림 신청이 완료되었습니다!
         </div>
-      )}
+      )} */}
+      <SubscribeModal
+        isOpen={isModalOpen}
+        topicTitle={topicData?.title ?? ''}
+        onClose={() => setIsModalOpen(false)}
+      />
 
-      <div className="px-6 md:px-12 pt-8 md:pt-10">
-        <div className="grid grid-cols-3 items-end pb-2">
-          <div className="text-left">
-            <p className="text-base md:text-lg font-medium tracking-wider text-[#333]">TIMELINE</p>
-          </div>
-          <div className="text-center">
-            <p className="text-base md:text-lg font-medium text-[#333]">
-              {topicData?.category ?? ''}
-            </p>
-          </div>
-          <div className="text-right">
-            <button
-              className="text-sm md:text-base font-medium text-[#555] hover:text-black transition-colors"
-              onClick={handleAlertClick}
-            >
-              <span className="hidden md:inline">실시간 알림받기</span>
-              <span className="md:hidden">알림받기</span>
-            </button>
-          </div>
-        </div>
-        <div className="border-b-[4px] border-[#222]"></div>
-        <div className="border-b-[1px] border-[#222] mt-[3px]"></div>
+      <NotificationPermissionModal
+        isOpen={showPermissionModal}
+        onClose={() => setShowPermissionModal(false)}
+      />
+
+      <div className="flex justify-between items-center border-b-4 border-gray47 pb-1 text-sm md:text-base">
+        <p className="text-sm mt-2 text-gray-500">
+          {topicData?.category ?? ''} &gt; [ 토픽 ] {topicData?.title ?? ''}
+        </p>
+        {/* <p className="text-base md:text-xl">{event.category}</p> */}
+        <button
+          className="bg-gray47 py-1 px-3 text-sm text-white rounded-full hover:bg-[#211D1E]"
+          onClick={handleSubscribe}
+        >
+          {subscribe ? '구독취소' : '타임라인 구독하기'}
+        </button>
       </div>
 
-      <h1 className="text-center text-[28px] md:text-[32px] font-bold mt-10 md:mt-12 mb-8 md:mb-10 tracking-tight">
-        {topicData?.title ?? ''}
-      </h1>
+      <div className="min-h-30 h-28 md:h-40 border-y-2 border-gray47 mt-2 text-xs flex flex-col items-center justify-center">
+        <p className="bg-gray47 text-white py-[3px] px-4 rounded-full flex items-center">토픽</p>
+        <h1 className="text-center text-[24px] md:text-[32px] text-base font-bold tracking-tight mt-4 mb-5">
+          {topicData?.title ?? ''}
+        </h1>
+      </div>
 
-      <div className="px-6 md:px-12 mb-5">
+      <div className="mb-5">
         <button
           onClick={toggleSortOrder}
-          className="flex items-center gap-1.5 bg-[#d9d9d9] px-3.5 py-1.5 border border-[#c4c4c4] rounded-sm shadow-sm hover:bg-[#d0d0d0] transition-colors inline-flex"
+          className="ml-2 my-5 text-white items-center gap-1.5 bg-[#575052] px-3.5 py-1.5 border rounded-md hover:bg-[#211D1E] transition-colors inline-flex"
         >
-          <div className="flex flex-col gap-[3px] text-[#222]">
+          <div className="flex flex-col gap-[3px] text-white">
             <svg
               width="10"
               height="10"
@@ -135,13 +188,11 @@ const TimelinePage = () => {
         </button>
       </div>
 
-      <div className="w-full border-t border-[#a0a0a0] mb-10 md:mb-12"></div>
-
-      <section className="flex flex-col lg:flex-row px-6 md:px-12 gap-12 lg:gap-10">
-        <div className="w-full lg:w-[32%] relative pl-2 lg:pl-4">
-          <div className="absolute left-[19px] lg:left-[27px] top-[14px] bottom-[14px] w-[2px] bg-[#474747] z-0"></div>
+      <section className="flex flex-col lg:flex-row lg:justify-between">
+        <div className="w-full lg:w-1/2 relative pl-2">
+          <div className="absolute left-[19px] lg:left-[19px] top-[14px] bottom-[14px] w-[2px] bg-[#474747] z-0"></div>
           <ul className="flex flex-col gap-8 md:gap-14 relative z-10 m-0 p-0 list-none">
-            {eventsdata?.events.map((item) => {
+            {eventsdata?.events.map((item, i) => {
               const isActive = item.id === selectedId;
               return (
                 <li key={item.id} className="flex items-start gap-4 md:gap-5 w-full">
@@ -152,19 +203,33 @@ const TimelinePage = () => {
                     type="button"
                     onClick={() => {
                       setSelectedId(item.id);
+
                       if (window.innerWidth < 1024) {
                         articleRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
                       }
                     }}
-                    className={`text-left break-words w-full transition-colors mt-[3px] ${
+                    className={`group text-left break-words w-full transition-colors mt-[3px] ${
                       isActive
-                        ? 'font-bold text-black'
-                        : 'font-medium text-gray-700 hover:text-black'
+                        ? 'font-bold text-gray47'
+                        : 'font-medium text-[#999999] hover:text-black'
                     }`}
                   >
-                    <span className="text-[17px] md:text-[20px] leading-tight block">
-                      {item.title}
-                    </span>
+                    <div className="text-md flex items-center gap-1 mb-1">
+                      <p
+                        className={`border px-2 fw-700 rounded-md h-[20px] text-xs flex items-center transition-colors
+                        ${
+                          isActive
+                            ? 'bg-[#474747] text-white border-[#474747]'
+                            : 'border-[#999999] text-[#999999] group-hover:border-black group-hover:text-black'
+                        }`}
+                      >
+                        이슈{i + 1}
+                      </p>
+
+                      <p>{item.created_at.slice(0, 10).replaceAll('-', '.')}</p>
+                    </div>
+
+                    <span className="text-[17px] md:text-lg leading-tight block">{item.title}</span>
                   </button>
                 </li>
               );
@@ -172,21 +237,23 @@ const TimelinePage = () => {
           </ul>
         </div>
 
-        <article ref={articleRef} className="w-full lg:w-[68%] flex flex-col lg:pr-12 min-w-0">
-          <img
-            src={activeItem?.event_image_url ?? undefined}
-            alt={activeItem?.title}
-            className="w-full aspect-video object-cover rounded-sm mb-8 md:mb-10 bg-[#ccc] shadow-sm transition-opacity duration-300"
-          />
-          <div className="text-base md:text-[18px] leading-relaxed md:leading-[32px] text-[#222] mb-12 font-medium w-full">
+        <article ref={articleRef} className="w-full lg:w-1/2 flex flex-col items-center min-w-0">
+          <div className="w-full mb-5">
+            <img
+              src={activeItem?.event_image_url ?? undefined}
+              alt={activeItem?.title}
+              className="w-full aspect-video object-cover rounded-sm md:mb-1 bg-[#ccc] shadow-sm transition-opacity duration-300"
+            />
+          </div>
+          <div className="text-[18px] leading-relaxed md:leading-[36px] text-gray47 mb-12 font-medium w-full">
             <p>{activeItem?.summary}</p>
           </div>
-          <div className="flex justify-end mt-auto">
+          <div className="mt-15 self-end bg-gray47 py-1 px-5 text-xs text-white rounded-full hover:bg-[#211D1E]">
             <Link
               to={`/event-detail/${activeItem?.id}`}
-              className="text-base md:text-lg font-bold text-gray-800 hover:text-gray-500 transition-colors flex items-center gap-1.5"
+              className="text-base md:text-md flex items-center"
             >
-              통계자료 같이 보기{' '}
+              이슈 상세 보기
               <span aria-hidden="true" className="font-normal">
                 →
               </span>
