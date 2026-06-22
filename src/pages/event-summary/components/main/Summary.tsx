a93653ac14b5d53341615c2d473318dc9af0149b
JJyen <gdb1632@naver.com>
2026-06-22 12:04:42 +0900

    fix: 빌드 오류 수정

--- src/pages/event-summary/components/main/Summary.tsx
+++ src/pages/event-summary/components/main/Summary.tsx
@@ -1,4 +1,4 @@
-import img from '@/assets/back1.jpg';
+import img from '@/assets/back.jpg';
 import type { EventSummary } from '@/types/eventSummary';
 
 const Summary = ({ event }: { event: EventSummary }) => {
